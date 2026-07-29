ca8dad91a39f51999ae20cb0bb5940a954c9a2b0
Unreal-endrawing <elizaissomewhatreal@gmail.com>
2026-07-28 21:09:51 -0600

    Milestone 1 official Upload
    *Can be run with the command python3 main.py
    *Added an Installer (built with tkinter), right now hardcoded to install to C:\Hexagono
    *Main.py has an interface now (built with PyQt, tkinter was really subpar) and isolates the provided slipper into output.png when the argentum button is clicked
    *Removing IMG_1309 and clicking the Argentum button will crash the app

---
+++ installer.py
@@ -0,0 +1,102 @@
+import os as os
+import shutil
+from tkinter import *
+from tkinter import ttk
+import runpy
+global hasruninstaller
+hasruninstaller = 0
+installerVer = "0.0.0 azucar" #Build number
+Guiboot = 1
+Main = r"C:\Hexagono" #install goes here
+Rawpics = r"C:\Hexagono\import" #Imported pictures as is go here
+Pics = r"C:\Hexagono\processed" #Pictures without background go here (Finished or not, not finished get marked for deletion)
+Saveflags = r"C:\Hexagono\registry" #Registry of program settings and sujects go here
+program = r"C:\Hexagono\Program" #Main.py (argentum and caron go here)
+Bc = "#" #Character to separate installer flags
+ErrorCodes = ["HEXAGONO is already installed, uninstall it and try again", "Unknown error"]
+def errorWindow(Code): #Console and gui error logging
+ print("Installer has found an error, with code i-")
+ print(Code)
+ print(ErrorCodes[Code])
+ Error = Tk()
+ Error.geometry("500x500")
+ Error.title("An error has ocurred, whoops")
+ ttk.Label(Error, text="An error has ocurred, with code i-").pack(pady=20)
+ ttk.Label(Error, text=Code).pack(pady=20)
+ ttk.Label(Error, text=ErrorCodes[Code]).pack(pady=20)
+ ttk.Button(Error, text="Quit", command=Error.destroy).pack(pady=20)
+ Error.mainloop()
+def SuccesWindow():
+  Finished = Tk()
+  Finished.geometry("500x500")
+  Finished.title("Installer is done")
+  Label(Finished, text="The installer has succesfully installed HEXAGONO in path ").pack(pady=20)
+  Label(Finished, text=Main).pack(pady=20)
+  Button(Finished, text="Quit", command=SuccesWindow.destroy).pack(pady=20) #Will figure out auto executing later
+
+  Button(Finished, text="Run hexagono", command=(runpy.run_path(r"C:\Hexagono\Program\main.py" ))).pack(pady=20)
+def isFlagsAthingyet():
+ global hasruninstaller
+ try: 
+  with open(r"C:\Hexagono\registry\Installerflags.txt") as Sr: #was temporary, now is permanent :(
+   print(Sr.read())
+   hasruninstaller = 1
+   errorWindow(0)
+ except OSError:
+  print("proceed")
+  InstallHexagono()
+ except IOError():
+  print("No previous install, proceeding...")
+  if hasruninstaller == 1:
+   errorWindow(0)
+  else:
+   print("No errors")
+  InstallHexagono()
+def InstallHexagono():
+ global hasruninstaller
+ if hasruninstaller == 0:
+      try:
+       os.makedirs(Main)
+       os.makedirs(Rawpics)
+       os.makedirs(Pics)
+       os.makedirs(Saveflags)
+       os.makedirs(program)
+       shutil.copyfile(r"main.py",r"C:\Hexagono\Program\main.py" )
+       shutil.copyfile(r"icon.png", r"C:\Hexagono\Program\icon.png")
+       hasruninstaller = 1
+       inst = str(hasruninstaller)
+       save = open(r"C:\Hexagono\registry\Installerflags.txt", "w",) 
+       save.write(inst)
+       save.write(Bc)
+       save.write(installerVer)
+       Installer.destroy
+       SuccesWindow()
+      except:
+       print("How r u here")
+       shutil.copyfile(r"main.py",r"C:\Hexagono\Program\main.py" )
+       shutil.copyfile(r"icon.png", r"C:\Hexagono\Program\icon.png")
+       hasruninstaller = 1
+       inst = str(hasruninstaller)
+       save = open(r"C:\Hexagono\registry\Installerflags.txt", "w")
+       save.write(inst)
+       save.write(Bc)
+       save.write(installerVer)
+       Installer.destroy
+
+
+
+ else: 
+  print("Error - Installer has been run already (Code i-0)")
+
+if Guiboot == 1:
+ Installer = Tk() #Create window for gui boot
+ Installer.geometry("1280x720") #Set resolution
+ Installer.title("H-E-X-A-G-O-N-O Setup version")
+
+ ttk.Label(Installer, text="Wellcome to HEXAGONO setup, this program will guide you through stup for HEXAGONO ").pack(pady=20)
+ ttk.Button(Installer, text="Install HEXAGONO", command=isFlagsAthingyet).pack(pady=20)
+ ttk.Button(Installer, text="Quit installer", command=Installer.destroy).pack(pady=20)
+
+
+
+Installer.mainloop()
--- main.py
+++ main.py
@@ -1,1 +1,143 @@
+from tkinter import *
+from tkinter import ttk #need to change to pyQt
+import runpy
+from pathlib import Path
+from PIL import Image
+import os as os
+from rembg import remove
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt6 import QtCore, QtGui
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QGridLayout,
+    QWidget,
+)
+import sys  #For command
+import PyQt6
+#TF am i doing
+PicTp = "IMG_1309.png"
+BuildVersion = "0.0.0 Azucar mascabado"
 
+#print("Welcome to HEXAGONO command line fallback, this is not the way it should be used, but anyways")
+#Code for the BackEnd goes here
+def GetSubjectPicture():
+    print("Not functional yet...")
+def removeBackground():
+    input = Image.open(PicTp)
+    output = remove(input)
+    output.save("Output.png")
+
+#UI Code Goes Here    
+class Mainwindows(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Hexagon QDDemo")
+        layout3 = QGridLayout()
+        self.label = QLabel("Hexagono GUI tool")
+        self.label.setFixedSize(100, 30)
+        self.setWindowIcon(QtGui.QIcon("icon.png"))
+        button = QPushButton("Argentum") #, currently pre-milestone 1; the future milestones are \n Milestone 1 - The main menu gets a layout \n Milestone 2 - Carbon is up and running \n Milestone 3 - Multiple File Output support \n Milestone 4 - File selection \n Milestone 5 - Color isolation \n Milestone 6 - Finished UI and graphics")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked) #Laugh at me following tutorials all ypu want
+        button.setFixedSize(QSize(100, 30))
+        Button2 = QPushButton("Carbon")
+        Button2.setCheckable(True)
+        Button2.clicked.connect(self.Button2_click)
+        Button2.setFixedSize(QSize(100, 30))
+        Button3 = QPushButton("Cool Stuf for the future")
+        Button3.clicked.connect(self.Button3_click)
+        VersionString = QLabel(BuildVersion)
+        VersionString.setFixedSize(QSize(140, 30))
+        layout3.addWidget(self.label, 0, 2)
+        layout3.addWidget(button, 1, 2)
+        layout3.addWidget(Button2, 2, 2 )
+        layout3.addWidget(Button3, 3, 2)
+        layout3.addWidget(VersionString, 4, 2 )
+        widget = QWidget()
+        widget.setLayout(layout3)
+        self.setCentralWidget(widget)
+        #self.setCentralWidget(button)
+        self.setFixedSize(QSize(1280, 700))
+    def the_button_was_clicked(self, checked):
+        removeBackground()
+        self.w = FileSelectArgentum()
+        self.w.show()
+    def Button2_click(self, checked):
+        self.x = CarbonEntryCreator()
+        self.x.show()
+    def Button3_click(self, checked):
+        self.y = FutureCoolStuf()
+        self.y.show()
+
+class FileSelectArgentum(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.label = QLabel("Argentum file select will go here in milestone 4!")
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+class CarbonEntryCreator(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout1 = QVBoxLayout()
+        self.label = QLabel("Carbon editor to e up and running in milestone 2!!")
+        layout1.addWidget(self.label)
+        self.setLayout(layout1)
+
+class FutureCoolStuf(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout4 = QVBoxLayout()
+        self.label = QLabel("Milestone 1 - A Menu you can click! - You are here")
+        self.label1 = QLabel("Milestone 2 - Carbon saves files")
+        self.label2 = QLabel("Milestone 3 - Now this is not just a slipper isolator")        
+        self.label3  = QLabel("Milestone 4 - Now you have File selection")
+        self.label4  = QLabel("Milestone 5 - Color isolator")
+        self.label5 = QLabel("Milestone 6 - Finishing touches for full relase")
+        layout4.addWidget(self.label)
+        layout4.addWidget(self.label1)
+        layout4.addWidget(self.label2)
+        layout4.addWidget(self.label3)
+        layout4.addWidget(self.label4)
+        layout4.addWidget(self.label5)        
+        self.setLayout(layout4)
+ #, currently pre-milestone 1; the future milestones are \n Milestone 1 - The main menu gets a layout \n Milestone 2 - Carbon is up and running \n Milestone 3 - Multiple File Output support \n Milestone 4 - File selection \n Milestone 5 - Color isolation \n Milestone 6 - Finished UI and graphics")
+        
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+app= QApplication([])
+#Create a window
+window = Mainwindows()
+window.show()#Show the window (Why hide it tho?)
+
+app.exec()
+#Installer = Tk() #Create window for gui boot
+#Installer.geometry("1280x720") #Set resolution
+#Installer.title("")
+#ttk.Label(Installer, text="Wellcome to HEXAGONO setup, this program will guide you through stup for HEXAGONO ").pack(pady=20)
+#ttk.Button(Installer, text="Install HEXAGONO", command=print("Hiiii")).pack(pady=20)
+#ttk.Button(Installer, text="Quit installer", command=Installer.destroy).pack(pady=20)
+
+
+#Installer.mainloop()
