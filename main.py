a785175201dfec4cd1b4f61f7c5c91d834902d4d
Unreal-endrawing <elizaissomewhatreal@gmail.com>
2026-07-29 23:43:58 -0600

    First beta of Milestone 2
    Rewritten (by changing less than 10 lines of code) to PySide in preparation for the milestone 6 propper UI

--- main.py
+++ main.py
@@ -5,37 +5,17 @@ from pathlib import Path
 from PIL import Image
 import os as os
 from rembg import remove
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
-from PyQt6 import QtCore, QtGui
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import (
-    QApplication,
-    QCheckBox,
-    QComboBox,
-    QDateEdit,
-    QDateTimeEdit,
-    QDial,
-    QDoubleSpinBox,
-    QFontComboBox,
-    QLabel,
-    QLCDNumber,
-    QLineEdit,
-    QMainWindow,
-    QProgressBar,
-    QPushButton,
-    QRadioButton,
-    QSlider,
-    QSpinBox,
-    QTimeEdit,
-    QVBoxLayout,
-    QGridLayout,
-    QWidget,
-)
+
 import sys  #For command
-import PyQt6
+
+from PySide6 import *
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+#from PySide6,Q
 #TF am i doing
 PicTp = "IMG_1309.png"
-BuildVersion = "0.0.0 Azucar mascabado"
+BuildVersion = "0.1.0 Azucar morena"
+CarbonIndexEntries = []
 
 #print("Welcome to HEXAGONO command line fallback, this is not the way it should be used, but anyways")
 #Code for the BackEnd goes here
@@ -45,8 +25,25 @@ def removeBackground():
     input = Image.open(PicTp)
     output = remove(input)
     output.save("Output.png")
-
-#UI Code Goes Here    
+def BackendCarbonRead():
+  with open(r"registry\CarbonIndex.txt") as Entries:
+    print(Entries)  #UI Code Goes Here    
+    PreList = Entries.read()
+    print(PreList)
+    CarbonIndexEntries = PreList.split()
+    print(CarbonIndexEntries)
+    if (len(CarbonIndexEntries) - 1 ) > 0:
+        for t in range(len(CarbonIndexEntries) - 1):
+         print("Here are the entries in the Carbon Editor")
+         print(t)
+         #Need to add edit function for existing entries
+    else:
+        print("hmm, no entries yet") 
+        print("Should we create an entry to start")
+        YesNoCreate = input("1 - yes, 2 - no")
+        if YesNoCreate == "1":
+            print("Need to add a CarbonEntryCreator() function upstream")
+#to be obliterated in milestone 6
 class Mainwindows(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -56,14 +53,14 @@ class Mainwindows(QMainWindow):
         self.label.setFixedSize(100, 30)
         self.setWindowIcon(QtGui.QIcon("icon.png"))
         button = QPushButton("Argentum") #, currently pre-milestone 1; the future milestones are \n Milestone 1 - The main menu gets a layout \n Milestone 2 - Carbon is up and running \n Milestone 3 - Multiple File Output support \n Milestone 4 - File selection \n Milestone 5 - Color isolation \n Milestone 6 - Finished UI and graphics")
-        button.setCheckable(True)
+        #button.setCheckable(True)
         button.clicked.connect(self.the_button_was_clicked) #Laugh at me following tutorials all ypu want
         button.setFixedSize(QSize(100, 30))
         Button2 = QPushButton("Carbon")
-        Button2.setCheckable(True)
+        #Button2.setCheckable(True)
         Button2.clicked.connect(self.Button2_click)
         Button2.setFixedSize(QSize(100, 30))
-        Button3 = QPushButton("Cool Stuf for the future")
+        Button3 = QPushButton("Cool Stuf for the future")#"Cool Stuf for the future"
         Button3.clicked.connect(self.Button3_click)
         VersionString = QLabel(BuildVersion)
         VersionString.setFixedSize(QSize(140, 30))
@@ -82,8 +79,7 @@ class Mainwindows(QMainWindow):
         self.w = FileSelectArgentum()
         self.w.show()
     def Button2_click(self, checked):
-        self.x = CarbonEntryCreator()
-        self.x.show()
+        BackendCarbonRead()
     def Button3_click(self, checked):
         self.y = FutureCoolStuf()
         self.y.show()
@@ -100,7 +96,7 @@ class CarbonEntryCreator(QWidget):
     def __init__(self):
         super().__init__()
         layout1 = QVBoxLayout()
-        self.label = QLabel("Carbon editor to e up and running in milestone 2!!")
+        self.label = QLabel("Carbon editor to be up and running in milestone 2!!")
         layout1.addWidget(self.label)
         self.setLayout(layout1)
 
@@ -108,8 +104,8 @@ class FutureCoolStuf(QWidget):
     def __init__(self):
         super().__init__()
         layout4 = QVBoxLayout()
-        self.label = QLabel("Milestone 1 - A Menu you can click! - You are here")
-        self.label1 = QLabel("Milestone 2 - Carbon saves files")
+        self.label = QLabel("Milestone 1 - A Menu you can click! - done")
+        self.label1 = QLabel("Milestone 2 - Carbon saves files - You are (almost) here")
         self.label2 = QLabel("Milestone 3 - Now this is not just a slipper isolator")        
         self.label3  = QLabel("Milestone 4 - Now you have File selection")
         self.label4  = QLabel("Milestone 5 - Color isolator")
